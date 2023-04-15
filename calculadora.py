2e510504f11a091dadb30b21a6a3a3ecdd19d62a
ggg21on <gomesgalikosky@gmail.com>
2023-04-15 17:41:38 -0300

    adjusting errors

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,7 @@
-import bleach
 import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 
-st = bleach.clean(st, tags=[], attritbutes={}, styles=[], strip=True)
 
 github_icon = '<a href="https://github.com/ggg21on" target="_blank"><img src="https://img.icons8.com/fluent/48/000000/github.png"/></a>'
 github_text = '<a href="https://github.com/ggg21on" target="_blank" style="font-size: 1.2em; margin-left: 0.5em;">Source Code</a>'
