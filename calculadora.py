22e97f3bcd26027fb4f04f291f667de3bfdf909c
ggg21on <gomesgalikosky@gmail.com>
2023-04-16 12:33:10 -0300

    add-videos

--- calculadora.py
+++ calculadora.py
@@ -64,7 +64,7 @@ df = pd.DataFrame(data)
 
 st.table(df)
 
-video_file = open('https://pixabay.com/en/videos/star-long-exposure-starry-sky-sky-6962/', 'rb')
+video_file = open('stars.mp4', 'rb')
 video_bytes = video_file.read()
 
 st.video(video_bytes)
