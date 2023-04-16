5f736ba0429ee292756bd37dc4c3ad0cf4cbfe66
ggg21on <gomesgalikosky@gmail.com>
2023-04-15 23:02:17 -0300

    fixing mistakes

--- calculadora.py
+++ calculadora.py
@@ -7,9 +7,7 @@ st.header('Numerical Base Conversion Calculator :computer:')
 # This subheader
 st.markdown('<h1 style="font-size:16px;">A number base calculator is a tool designed to perform mathematical calculations involving numbers in different number systems, such as the decimal, binary, octal, or hexadecimal system.</h1>', unsafe_allow_html=True)
 
-
-bases = ['Binary', 'Octal', 'Decimal', 'Hexadecimal', 'Sexagesimal']
-
+bases = ['Binary', 'Octal', 'Decimal', 'Hexadecimal']
 
 # Selection
 entrada_base = st.selectbox('Select input base:', bases)
@@ -33,9 +31,6 @@ if st.button('Convert'):
         elif entrada_base == 'Hexadecimal':
             valor_decimal = int(str(entrada_valor), 16)
 
-        elif entrada_base == 'Sexagesimal':
-            valor_decimal = int(str(entrada_valor), 60)
-
         # Converter decimal para saída
         if saida_base == 'Binary':
             valor_saida = bin(valor_decimal)[2:]
@@ -49,23 +44,20 @@ if st.button('Convert'):
         elif saida_base == 'Hexadecimal':
             valor_saida = hex(valor_decimal)[2:]
 
-        elif saida_base == 'Sexagesimal':
-            valor_saida = divmod(valor_decimal)[2:]
-
         # Exibir resultado
-        st.write(f'The value {entrada_valor} at the base {entrada_base} is the same as {valor_saida}  at the base {saida_base}')
-        
+        st.write(
+            f'The value {entrada_valor} at the base {entrada_base} is the same as {valor_saida}  at the base {saida_base}')
+
     except ValueError:
         st.write('Input value is invalid for selected base!',
                  unsafe_allow_html=True)
-
+# title table
 st.subheader('Equivalence Table')
 # table
 decimals = list(range(17))
 octals = [oct(i)[2:] for i in decimals]
 binaries = [bin(i)[2:].zfill(5) for i in decimals]
 hexadecimals = [hex(i)[2:].upper() for i in decimals]
-sexagesimals = [divmod(i)[2:].upper() for i in decimals]
 
 data = {'Decimal': decimals, 'Octal': octals,
         'Binary': binaries, 'Hexadecimal': hexadecimals}
