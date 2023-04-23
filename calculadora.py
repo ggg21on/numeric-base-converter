e24c8568f7cec48cfeec07a43adf1b013ff0ca46
ggg21on <gomesgalikosky@gmail.com>
2023-04-23 20:35:03 -0300

    calcu_num

--- calculadora.py
+++ calculadora.py
@@ -11,44 +11,56 @@ bases = ['Binary', 'Octal', 'Decimal', 'Hexadecimal']
 
 # Selection
 entrada_base = st.selectbox('Select input base:', bases)
-entrada_valor = st.number_input('Select input value:', step=1)
+entrada_valor = st.text_input(
+    'Select input value:', value='', max_chars=None, key=None)
 
 saida_base = st.selectbox('Select outpat basis', bases)
 
-# Input
+
+def is_valid_digited(digited, base):
+    valid_digits = {
+        'Binary': ['0', '1'],
+        'Octal': ['0', '1', '2', '3', '4', '5', '6', '7'],
+        'Decimal': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+        'Hexadecimal': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+    }
+    return digited.upper() in valid_digits[base]
+
+
 if st.button('Convert'):
-    try:
-        # Convert input to decimal
-        if entrada_base == 'Binary':
-            valor_decimal = int(str(entrada_valor), 2)
+    if is_valid_digited(entrada_valor, entrada_base):
+        try:
+            # Convert input to decimal
+            if entrada_base == 'Binary':
+                valor_decimal = int(str(entrada_valor), 2)
 
-        elif entrada_base == 'Octal':
-            valor_decimal = int(str(entrada_valor), 8)
+            elif entrada_base == 'Octal':
+                valor_decimal = int(str(entrada_valor), 8)
 
-        elif entrada_base == 'Decimal':
-            valor_decimal = int(str(entrada_valor))
+            elif entrada_base == 'Decimal':
+                valor_decimal = int(str(entrada_valor))
 
-        elif entrada_base == 'Hexadecimal':
-            valor_decimal = int(str(entrada_valor), 16)
+            elif entrada_base == 'Hexadecimal':
+                valor_decimal = int(str(entrada_valor), 16)
 
-        # Convert decimal to output
-        if saida_base == 'Binary':
-            valor_saida = bin(valor_decimal)[2:]
+            # Convert decimal to output
+            if saida_base == 'Binary':
+                valor_saida = bin(valor_decimal)[2:]
 
-        elif saida_base == 'Octal':
-            valor_saida = oct(valor_decimal)[2:]
+            elif saida_base == 'Octal':
+                valor_saida = oct(valor_decimal)[2:]
 
-        elif saida_base == 'Decimal':
-            valor_saida = str(valor_decimal)
+            elif saida_base == 'Decimal':
+                valor_saida = str(valor_decimal)
 
-        elif saida_base == 'Hexadecimal':
-            valor_saida = hex(valor_decimal)[2:]
+            elif saida_base == 'Hexadecimal':
+                valor_saida = hex(valor_decimal)[2:]
 
-        # Show result
-        st.write(
-            f'The value {entrada_valor} at the base {entrada_base} is the same as {valor_saida}  at the base {saida_base}')
+            # Show result
+            st.success(
+                f'The value {entrada_valor} at the base {entrada_base} is the same as {valor_saida}  at the base {saida_base}')
 
-    except ValueError:
+        except ValueError:
             st.markdown('Input value is invalid for selected base!')
 
 # title table equivalence
@@ -59,12 +71,8 @@ decimals = list(range(17))
 octals = [oct(i)[2:] for i in decimals]
 binaries = [bin(i)[2:].zfill(5) for i in decimals]
 hexadecimals = [hex(i)[2:].upper() for i in decimals]
-
 data = {'Decimal': decimals, 'Octal': octals,
         'Binary': binaries, 'Hexadecimal': hexadecimals}
 df = pd.DataFrame(data)
 
 st.table(df)
-
-
-
