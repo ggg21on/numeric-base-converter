60d1e1f9cd33fe9db0751ef5eb585705165b914b
ggg21on <gomesgalikosky@gmail.com>
2023-04-23 22:00:04 -0300

    update

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import streamlit.components.v1 as components
 
 # This is header
 st.header('Numerical Base Conversion Calculator :computer:')
@@ -11,11 +10,9 @@ bases = ['Binary', 'Octal', 'Decimal', 'Hexadecimal']
 
 # Selection
 entrada_base = st.selectbox('Select input base:', bases)
-entrada_valor = st.text_input(
-    'Select input value:', value='', max_chars=None, key=None)
+entrada_valor = st.text_input('Select input value:', max_chars=None, key=None)
 
 saida_base = st.selectbox('Select outpat basis', bases)
-
 
 def is_valid_digited(digited, base):
     valid_digits = {
@@ -26,9 +23,9 @@ def is_valid_digited(digited, base):
     }
     return digited.upper() in valid_digits[base]
 
-
+# Input
 if st.button('Convert'):
-    if is_valid_digited(entrada_valor, entrada_base):
+    if all([is_valid_digited(d, entrada_base) for d in entrada_valor]):
         try:
             # Convert input to decimal
             if entrada_base == 'Binary':
@@ -71,8 +68,39 @@ decimals = list(range(17))
 octals = [oct(i)[2:] for i in decimals]
 binaries = [bin(i)[2:].zfill(5) for i in decimals]
 hexadecimals = [hex(i)[2:].upper() for i in decimals]
+
 data = {'Decimal': decimals, 'Octal': octals,
         'Binary': binaries, 'Hexadecimal': hexadecimals}
 df = pd.DataFrame(data)
 
 st.table(df)
+footer="""<meta name="viewport" content="width=device-width, initial-scale=1">
+ <style>
+    img {
+      max-width: 100%;
+    }
+    .footer {
+      position: static;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: auto;
+      text-align: center;
+    }
+    .footer img {
+      filter: invert(1);
+    }
+     a[href="https://github.com/ggg21on/numeric-base-converter"] img {
+    filter: invert(32%) sepia(99%) saturate(465%) hue-rotate(194deg) brightness(102%) contrast(101%);
+  }
+  </style>
+  <div class="footer">
+    <br><br><br><br>
+    <p><b>Get In Touch With Me</b></p>
+    <a href="https://github.com/ggg21on/numeric-base-converter"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+    alt="github icon" width="50" height="50"></a>
+    <a href="https://www.linkedin.com/in/gabriel-gomes-574287258/?locale=en_US"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+    alt="linkedin icon" width="50" height="50"></a>
+  </div>
+   """
+st.markdown(footer,unsafe_allow_html=True)
